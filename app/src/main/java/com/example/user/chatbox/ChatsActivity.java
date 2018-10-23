2b0db519f69aafd559f0b03c2d86ddaf274f517b
Sonu-s <sonus2884@gmail.com>
2018-10-23 17:16:46 +0530

    Edited

--- app/src/main/java/com/example/user/chatbox/ChatsActivity.java
+++ app/src/main/java/com/example/user/chatbox/ChatsActivity.java
@@ -2,41 +2,41 @@ package com.example.user.chatbox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatsActivity extends AppCompatActivity {
 
     private EditText messageText;
-   private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager lm;
-    private RecyclerView.Adapter mAdapter;
-    private List<String> message;
     private ImageButton sendButton;
     private FirebaseAuth mAuth;
     private DatabaseReference mReferenceSend;
     private DatabaseReference mReferenceReceive;
 
-    private List<SendReceive> msg;
-    private List<SendReceive> receives;
-
+    private Map map;
+    private GenericTypeIndicator<Map<String, String>> genericTypeIndicator;
+    private LinearLayout layout;
+    private ScrollView scrollView;
 
 
     @Override
@@ -44,8 +44,10 @@ public class ChatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
 
+        layout = findViewById(R.id.layout1);
+        scrollView = findViewById(R.id.scrollView);
         messageText = findViewById(R.id.messageText);
-       recyclerView = findViewById(R.id.recyclerView);
+        // recyclerView = findViewById(R.id.recyclerView);
         sendButton = findViewById(R.id.sendImage);
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -59,13 +61,10 @@ public class ChatsActivity extends AppCompatActivity {
             }
         });
 
-        msg = new ArrayList<>();
-        receives = new ArrayList<>();
+        map = new HashMap();
+        genericTypeIndicator = new GenericTypeIndicator<Map<String, String>>() {
+        };
 
-        lm = new LinearLayoutManager(this);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(lm);
-        message = new ArrayList<>();
         mAuth = FirebaseAuth.getInstance();
         mReferenceSend = FirebaseDatabase.getInstance().getReference("Messages")
                 .child(MainActivity.name + "_" + RecyclerViewAdapter.name1);
@@ -113,13 +112,14 @@ public class ChatsActivity extends AppCompatActivity {
     public void send(View view) {
         String msg = messageText.getText().toString().trim();
 
-        message.add(msg);
+        Map<String, String> map = new HashMap<>();
+        map.put("message", msg);
+        map.put("user", MainActivity.name);
 
-        SendReceive sendReceive = new SendReceive();
-        sendReceive.setSendSms(msg);
-        mReferenceSend.push().setValue(sendReceive);
-        //  mReferenceReceive.push().setValue(sendReceive);
+        mReferenceSend.push().setValue(map);
+        mReferenceReceive.push().setValue(map);
         messageText.setText("");
+
         retrieveMsg();
 
     }
@@ -132,39 +132,32 @@ public class ChatsActivity extends AppCompatActivity {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                msg.clear();
+                // msg.clear();
+
+
+                map.clear();
 
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
 
-                    SendReceive sendReceive = ds.getValue(SendReceive.class);
-                    msg.add(sendReceive);
+                    map = ds.getValue(genericTypeIndicator);
 
-                }
-                mAdapter = new SendReceiveAdapter(ChatsActivity.this, msg, receives);
-               recyclerView.setAdapter(mAdapter);
+                    String message = map.get("message").toString();
+                    String user = map.get("user").toString();
 
-            }
+                    if (user.equals(MainActivity.name)) {
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                        Log.i("_msg", message);
+                        Log.i("_user", user);
+                        addMessageBox(message, 1);
 
-            }
-        });
 
-        mReferenceReceive.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                receives.clear();
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    } else {
 
-                    SendReceive receive = ds.getValue(SendReceive.class);
-                    receives.add(receive);
-
+                        addMessageBox(message, 2);
+                    }
 
                 }
 
-                mAdapter = new SendReceiveAdapter(ChatsActivity.this, msg, receives);
-                recyclerView.setAdapter(mAdapter);
 
             }
 
@@ -175,6 +168,33 @@ public class ChatsActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void addMessageBox(String message, int type) {
+        TextView textView = new TextView(ChatsActivity.this);
+        textView.setText(message);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+
+        if (type == 1) {
+            lp.setMargins(0, 8, 8, 8);
+            lp.gravity = Gravity.END;
+
+            textView.setLayoutParams(lp);
+            textView.setTextSize(18f);
+            textView.setBackgroundResource(R.drawable.send_bg);
+        } else {
+            lp.setMargins(8, 8, 0, 8);
+            lp.gravity = Gravity.START;
+
+            textView.setLayoutParams(lp);
+            textView.setTextSize(18f);
+            textView.setBackgroundResource(R.drawable.receive_bg);
+        }
+
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
     }
 
 }
